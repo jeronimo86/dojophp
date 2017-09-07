02776f6d71639bfe090930fe731dff783eb47aac
Jeronimo, Aleixo A <jeronimo86@users.noreply.github.com>
2017-09-07 15:50:52 +0100

    Continuando os teste do Commit

--- exercicio.php
+++ exercicio.php
@@ -1,11 +1,13 @@
 <?php
 
-define (meu_nome, "AjA", false);
+ // @author: Jeronimo, A
+
 $dia_nascimento = 29;
 $mes_nascimento = "Setembro";
 $ano_nascimento = 1980;
 $ano_actual = 2017;
 
+// Funcao para calcular idade
 function idade_usuario() {
     global $ano_actual;
     global $ano_nascimento;
@@ -14,7 +16,18 @@ function idade_usuario() {
 }
 
 idade_usuario();
-echo meu_nome;
+
+/**
+ 
+ @author: Jeronimo, A
+ Uma classe para calcular a idade
+ 
+ */
+
+namespace DojoPHP\desafios;
+
+class calculadora_de_idade
+
 
 
 
