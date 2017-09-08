7444cebe95c8b67bf1a51872cfb2f13e0d303765
Jeronimo, Aleixo A <jeronimo86@users.noreply.github.com>
2017-09-08 12:59:37 +0100

    Merge branch 'master' of https://github.com/jeronimo86/dojophp into calculadora-de-idade
    Calculo Feito Para Correcao

--- src/Calculadora.php
+++ src/Calculadora.php
@@ -4,16 +4,29 @@ namespace DojoPHP\Desafios;
 
 class Calculadora {
 
-    /* Definir a funcao*/
+   /* Declarando as info do usuario*/
+   
+      $data = '08/09/2017';
+    
+    /* Lista das Variaveis*/
+   
+       list( $dia, $mes, $ano) = explode('/', $data);
+    
+    /* Descrever */
+   
+       $hoje = mktime(0, 0, 0, date('d'), date('m'), date('Y'));
+    
+       $nascimento = mktime(0, 0, 0, $dia, $mes, $ano);
+    
+    //Funcao para calcular a Idade
 
-	public function calcularIdade() {
+    public function calcularIdade() {
       
-	 echo "Quantos Anos Tens?";
+    floor ((((($hoje - $nascimento) /60) / 60) / 24) / 365);
+	  
+	return calcularIdade();
 	 
-	}
-
-calcularIdade();
+	 }	
 	
 }
 
-?>
